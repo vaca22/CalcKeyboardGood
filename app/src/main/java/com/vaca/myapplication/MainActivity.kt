8f4bfa94c826bf33bca2b848c1a638e3dc363a9b
liguanghao <liguanghao@viatomtech.com>
2022-11-23 09:35:07 +0800

    jkljk

--- app/src/main/java/com/vaca/myapplication/MainActivity.kt
+++ app/src/main/java/com/vaca/myapplication/MainActivity.kt
@@ -3,13 +3,16 @@ package com.vaca.myapplication
 import android.os.Bundle
 import android.os.Looper
 import android.os.Message
+import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 import com.vaca.myapplication.calc.*
 
 class MainActivity : AppCompatActivity(),  MessageListener {
+    lateinit var textView:TextView
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        textView=findViewById(R.id.cc)
         MyApplication.addMessageListener(this)
     }
     private val mWeakHandler: WeakHandler = WeakHandler(Looper.getMainLooper())
@@ -22,7 +25,7 @@ class MainActivity : AppCompatActivity(),  MessageListener {
                 val keyValue: String = keyBoardEvent.getKeyValue()
                 val keyCode: Byte = keyBoardEvent.getKeyCode()
                 LogUtil.e("keyvalue=$keyValue")
-
+                textView.text=keyValue
             }
         }
     }
